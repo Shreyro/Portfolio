9a7ed2ecf2def7007d9e661ae3acf4266da1b13a
Shreyro <91416797+Shreyro@users.noreply.github.com>
2024-02-21 16:27:38 +0000

    commit1

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Shreya Roshan",
+  title: "AI & ML Engineer",
+  email: "shreya.roshan2021@gmail.com",
+  gitHub: "https://github.com/Shreyro",
+  linkedIn: "https://www.linkedin.com/in/shreya-roshan/",
 };
 
 const primaryColor = "#4E567E";
