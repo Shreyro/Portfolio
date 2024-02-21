ab928df7e559e7a8aae0cc3b4d1dc4c100d15b7f
Shreyro <91416797+Shreyro@users.noreply.github.com>
2024-02-21 17:47:04 +0000

    undo

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/5.jpeg";
+import image from "../images/design-desk.jpeg";
 
-const imageAltText = "girl with books and laptop";
+const imageAltText = "desktop with books and laptop";
 
 /**
  * Project list
